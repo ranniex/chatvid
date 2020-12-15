1a65439490f35150285248220f934be6824545bd
Ranniex <deporsam@hotmail.com>
2020-12-14 23:37:08 -0500

    missing db

---
+++ src/components/Input/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = ({ message, setMessage, sendMessage }) => (
+    <form className="form">
+    <input 
+        className="input"
+        type="text"
+        placeholder="Escribe un mensaje..."
+        value={message}
+        onChange={(event)=> setMessage(event.target.value)}
+        onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+    /> 
+    <button className="sendButton" onClick={(event) => sendMessage(event)}>Enviar</button>
+    </form>
+
+    
+);
+
+export default Input;
